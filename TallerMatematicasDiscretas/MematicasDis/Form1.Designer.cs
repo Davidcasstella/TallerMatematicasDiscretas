5f4f7b57322eea2bf3c8dce0bd21d1bb055f3a7a
Davidcasstella <brayan.lopez07@uptc.edu.co>
2025-04-05 00:08:56 -0500

    comit

---
+++ TallerMatematicasDiscretas/MematicasDis/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace MematicasDis
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            Hora = new System.Windows.Forms.Timer(components);
+            Lahora = new Label();
+            ComedorTemp = new TrackBar();
+            HabitacionTemp = new TrackBar();
+            SalaTemp = new TrackBar();
+            CocinaTemp = new TrackBar();
+            PuertaComedor = new CheckBox();
+            PuertaHabitacion = new CheckBox();
+            PuertaCocina = new CheckBox();
+            PuertaSala = new CheckBox();
+            btnBombilloComedor = new Button();
+            btnBombilloHabitacion = new Button();
+            btnBombilloCocina = new Button();
+            btnBombilloSala = new Button();
+            VentanaComedor = new CheckBox();
+            VentanaHabitacion = new CheckBox();
+            VentanaCocina = new CheckBox();
+            VentanaSala = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)ComedorTemp).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)HabitacionTemp).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)SalaTemp).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)CocinaTemp).BeginInit();
+            SuspendLayout();
+            // 
+            // Hora
+            // 
+            Hora.Enabled = true;
+            Hora.Tick += Hora_Tick;
+            // 
+            // Lahora
+            // 
+            Lahora.AutoSize = true;
+            Lahora.BackColor = Color.SpringGreen;
+            Lahora.Font = new Font("Segoe UI", 26F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Lahora.Location = new Point(467, 38);
+            Lahora.Name = "Lahora";
+            Lahora.Size = new Size(142, 70);
+            Lahora.TabIndex = 0;
+            Lahora.Text = "Reloj";
+            Lahora.Click += Lahora_Click;
+            // 
+            // ComedorTemp
+            // 
+            ComedorTemp.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            ComedorTemp.Cursor = Cursors.Cross;
+            ComedorTemp.Location = new Point(89, 38);
+            ComedorTemp.Name = "ComedorTemp";
+            ComedorTemp.Orientation = Orientation.Vertical;
+            ComedorTemp.Size = new Size(69, 156);
+            ComedorTemp.TabIndex = 1;
+            ComedorTemp.Scroll += ComedorTemp_Scroll;
+            // 
+            // HabitacionTemp
+            // 
+            HabitacionTemp.Location = new Point(665, 38);
+            HabitacionTemp.Name = "HabitacionTemp";
+            HabitacionTemp.Orientation = Orientation.Vertical;
+            HabitacionTemp.Size = new Size(69, 156);
+            HabitacionTemp.TabIndex = 2;
+            // 
+            // SalaTemp
+            // 
+            SalaTemp.Location = new Point(665, 289);
+            SalaTemp.Name = "SalaTemp";
+            SalaTemp.Orientation = Orientation.Vertical;
+            SalaTemp.Size = new Size(69, 156);
+            SalaTemp.TabIndex = 3;
+            // 
+            // CocinaTemp
+            // 
+            CocinaTemp.Location = new Point(89, 289);
+            CocinaTemp.Name = "CocinaTemp";
+            CocinaTemp.Orientation = Orientation.Vertical;
+            CocinaTemp.Size = new Size(69, 156);
+            CocinaTemp.TabIndex = 4;
+            // 
+            // PuertaComedor
+            // 
+            PuertaComedor.AutoSize = true;
+            PuertaComedor.Location = new Point(164, 38);
+            PuertaComedor.Name = "PuertaComedor";
+            PuertaComedor.Size = new Size(163, 29);
+            PuertaComedor.TabIndex = 5;
+            PuertaComedor.Text = "PuertaComedor";
+            PuertaComedor.UseVisualStyleBackColor = true;
+            PuertaComedor.CheckedChanged += PuertaComedor_CheckedChanged;
+            // 
+            // PuertaHabitacion
+            // 
+            PuertaHabitacion.AutoSize = true;
+            PuertaHabitacion.Location = new Point(834, 38);
+            PuertaHabitacion.Name = "PuertaHabitacion";
+            PuertaHabitacion.Size = new Size(173, 29);
+            PuertaHabitacion.TabIndex = 6;
+            PuertaHabitacion.Text = "PuertaHabitacion";
+            PuertaHabitacion.UseVisualStyleBackColor = true;
+            PuertaHabitacion.CheckedChanged += PuertaHabitacion_CheckedChanged;
+            // 
+            // PuertaCocina
+            // 
+            PuertaCocina.AutoSize = true;
+            PuertaCocina.Location = new Point(164, 257);
+            PuertaCocina.Name = "PuertaCocina";
+            PuertaCocina.Size = new Size(141, 29);
+            PuertaCocina.TabIndex = 7;
+            PuertaCocina.Text = "PuertaCocina";
+            PuertaCocina.UseVisualStyleBackColor = true;
+            PuertaCocina.CheckedChanged += PuertaCocina_CheckedChanged;
+            // 
+            // PuertaSala
+            // 
+            PuertaSala.AutoSize = true;
+            PuertaSala.Location = new Point(834, 247);
+            PuertaSala.Name = "PuertaSala";
+            PuertaSala.Size = new Size(120, 29);
+            PuertaSala.TabIndex = 8;
+            PuertaSala.Text = "PuertaSala";
+            PuertaSala.UseVisualStyleBackColor = true;
+            PuertaSala.CheckedChanged += PuertaSala_CheckedChanged;
+            // 
+            // btnBombilloComedor
+            // 
+            btnBombilloComedor.Font = new Font("Segoe UI", 28F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnBombilloComedor.Location = new Point(150, 73);
+            btnBombilloComedor.Name = "btnBombilloComedor";
+            btnBombilloComedor.Size = new Size(61, 92);
+            btnBombilloComedor.TabIndex = 9;
+            btnBombilloComedor.Text = "BombilloComedor";
+            btnBombilloComedor.UseVisualStyleBackColor = true;
+            btnBombilloComedor.Click += btnBombillo_Click;
+            // 
+            // btnBombilloHabitacion
+            // 
+            btnBombilloHabitacion.Font = new Font("Segoe UI", 28F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnBombilloHabitacion.Location = new Point(740, 73);
+            btnBombilloHabitacion.Name = "btnBombilloHabitacion";
+            btnBombilloHabitacion.Size = new Size(61, 92);
+            btnBombilloHabitacion.TabIndex = 10;
+            btnBombilloHabitacion.Text = "BombilloHabitacion";
+            btnBombilloHabitacion.UseVisualStyleBackColor = true;
+            btnBombilloHabitacion.Click += btnBombilloHabitacion_Click;
+            // 
+            // btnBombilloCocina
+            // 
+            btnBombilloCocina.Font = new Font("Segoe UI", 28F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnBombilloCocina.Location = new Point(164, 324);
+            btnBombilloCocina.Name = "btnBombilloCocina";
+            btnBombilloCocina.Size = new Size(61, 93);
+            btnBombilloCocina.TabIndex = 11;
+            btnBombilloCocina.Text = "BombilloCocina";
+            btnBombilloCocina.UseVisualStyleBackColor = true;
+            btnBombilloCocina.Click += btnBombilloCocina_Click;
+            // 
+            // btnBombilloSala
+            // 
+            btnBombilloSala.Font = new Font("Segoe UI", 28F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnBombilloSala.Location = new Point(725, 333);
+            btnBombilloSala.Name = "btnBombilloSala";
+            btnBombilloSala.Size = new Size(57, 84);
+            btnBombilloSala.TabIndex = 12;
+            btnBombilloSala.Text = "BombilloSala";
+            btnBombilloSala.UseVisualStyleBackColor = true;
+            btnBombilloSala.Click += btnBombilloSala_Click_1;
+            // 
+            // VentanaComedor
+            // 
+            VentanaComedor.AutoSize = true;
+            VentanaComedor.Location = new Point(164, 12);
+            VentanaComedor.Name = "VentanaComedor";
+            VentanaComedor.Size = new Size(176, 29);
+            VentanaComedor.TabIndex = 13;
+            VentanaComedor.Text = "VentanaComedor";
+            VentanaComedor.UseVisualStyleBackColor = true;
+            VentanaComedor.CheckedChanged += VentanaComedor_CheckedChanged;
+            // 
+            // VentanaHabitacion
+            // 
+            VentanaHabitacion.AutoSize = true;
+            VentanaHabitacion.Location = new Point(834, 12);
+            VentanaHabitacion.Name = "VentanaHabitacion";
+            VentanaHabitacion.Size = new Size(186, 29);
+            VentanaHabitacion.TabIndex = 14;
+            VentanaHabitacion.Text = "VentanaHabitacion";
+            VentanaHabitacion.UseVisualStyleBackColor = true;
+            VentanaHabitacion.CheckedChanged += VentanaHabitacion_CheckedChanged;
+            // 
+            // VentanaCocina
+            // 
+            VentanaCocina.AutoSize = true;
+            VentanaCocina.Location = new Point(164, 231);
+            VentanaCocina.Name = "VentanaCocina";
+            VentanaCocina.Size = new Size(154, 29);
+            VentanaCocina.TabIndex = 15;
+            VentanaCocina.Text = "VentanaCocina";
+            VentanaCocina.UseVisualStyleBackColor = true;
+            VentanaCocina.CheckedChanged += VentanaCocina_CheckedChanged;
+            // 
+            // VentanaSala
+            // 
+            VentanaSala.AutoSize = true;
+            VentanaSala.Location = new Point(834, 222);
+            VentanaSala.Name = "VentanaSala";
+            VentanaSala.Size = new Size(133, 29);
+            VentanaSala.TabIndex = 16;
+            VentanaSala.Text = "VentanaSala";
+            VentanaSala.UseVisualStyleBackColor = true;
+            VentanaSala.CheckedChanged += VentanaSala_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1179, 591);
+            Controls.Add(VentanaSala);
+            Controls.Add(VentanaCocina);
+            Controls.Add(VentanaHabitacion);
+            Controls.Add(VentanaComedor);
+            Controls.Add(btnBombilloSala);
+            Controls.Add(btnBombilloCocina);
+            Controls.Add(btnBombilloHabitacion);
+            Controls.Add(btnBombilloComedor);
+            Controls.Add(PuertaHabitacion);
+            Controls.Add(PuertaSala);
+            Controls.Add(PuertaCocina);
+            Controls.Add(PuertaComedor);
+            Controls.Add(CocinaTemp);
+            Controls.Add(SalaTemp);
+            Controls.Add(HabitacionTemp);
+            Controls.Add(ComedorTemp);
+            Controls.Add(Lahora);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)ComedorTemp).EndInit();
+            ((System.ComponentModel.ISupportInitialize)HabitacionTemp).EndInit();
+            ((System.ComponentModel.ISupportInitialize)SalaTemp).EndInit();
+            ((System.ComponentModel.ISupportInitialize)CocinaTemp).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Hora;
+        private Label Lahora;
+        private TrackBar ComedorTemp;
+        private TrackBar HabitacionTemp;
+        private TrackBar SalaTemp;
+        private TrackBar CocinaTemp;
+        private CheckBox PuertaComedor;
+        private CheckBox PuertaHabitacion;
+        private CheckBox PuertaCocina;
+        private CheckBox PuertaSala;
+        private Button btnBombilloComedor;
+        private Button btnBombilloHabitacion;
+        private Button btnBombilloCocina;
+        private Button btnBombilloSala;
+        private CheckBox VentanaComedor;
+        private CheckBox VentanaHabitacion;
+        private CheckBox VentanaCocina;
+        private CheckBox VentanaSala;
+    }
+}
---
+++ TallerMatematicasDiscretas/MematicasDis/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MematicasDis
+{
+    public partial class Form1 : Form
+
+
+    {
+        private System.Timers.Timer timer;
+        private int secondsPassed = 0;
+        private int millisecondsPassed = 0;
+        private int microsecondsPassed = 0;
+        private bool bombilloEncendido = false;
+        private Image bombilloApagado;
+        private Image bombilloEncendidoImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000; // 1 segundo
+            Hora.Tick += Hora_Tick;
+            secondsPassed = 0;
+            btnBombilloComedor.Text = "💡 Apagar";
+            btnBombilloHabitacion.Text = "💡 Apagar";
+            btnBombilloCocina.Text = "💡 Apagar";
+            btnBombilloSala.Text = "💡 Apagar";
+
+
+        }
+
+
+        private void Lahora_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+
+        }
+
+
+
+        private void Hora_Tick(object sender, EventArgs e)
+        {
+            secondsPassed++;
+            // Un día real tiene 86400 segundos. Dividimos el día en 24 segundos.
+            int totalSecondsInDay = 60 * 60; // Total de segundos en un día real
+            int totalMillisecondsInDay = (60 * 24) / 1000;
+
+            // Calcular el equivalente en segundos de acuerdo a los segundos transcurridos
+            int equivalentSeconds = (secondsPassed * totalSecondsInDay) / 24;
+            //int equivalentMilliseconds = (secondsPassed * totalMillisecondsInDay);// Cada segundo del temporizador representa 1/24 de un día real
+
+            // Convertir el equivalente de segundos en horas, minutos y segundos
+            int customHours = equivalentSeconds / 3600; // Convertir segundos a horas
+            int customMinutes = (equivalentSeconds % 3600) / 60; // Convertir el residuo de segundos en minutos
+            int custommili = (equivalentSeconds % 999) / 11;
+
+            // Mostrar el tiempo en formato HH:MM:SS
+            string customTimeString = string.Format("{0:D2}:{1:D2}:{2:D2}", customHours, customMinutes, custommili);
+
+            // Actualizar el label con el formato adecuado
+            Lahora.Text = customTimeString;
+
+            // Detener el temporizador cuando llegamos a 24 segundos (un "día")
+            if (secondsPassed <= 24)
+            {
+                timer.Stop();  // Detener el temporizador después de 24 segundos
+            }
+        }
+
+
+        private void btnBombillo_Click(object sender, EventArgs e)
+        {
+            {
+                if (bombilloEncendido)
+                {
+                    // Apagar el bombillo (simbolizado por el icono apagado)
+                    btnBombilloComedor.Text = "💡";
+                    btnBombilloComedor.ForeColor = Color.Gray;
+                }
+                else
+                {
+                    // Encender el bombillo (color amarillo)
+                    btnBombilloComedor.Text = "💡   ";
+                    btnBombilloComedor.ForeColor = Color.Yellow;
+                }
+
+                bombilloEncendido = !bombilloEncendido; // Cambiar el estado
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBombilloHabitacion_Click(object sender, EventArgs e)
+        {
+            {
+                if (bombilloEncendido)
+                {
+                    // Apagar el bombillo (simbolizado por el icono apagado)
+                    btnBombilloHabitacion.Text = "💡";
+                    btnBombilloHabitacion.ForeColor = Color.Gray;
+                }
+                else
+                {
+                    // Encender el bombillo (color amarillo)
+                    btnBombilloHabitacion.Text = "💡   ";
+                    btnBombilloHabitacion.ForeColor = Color.Yellow;
+                }
+
+                bombilloEncendido = !bombilloEncendido; // Cambiar el estado
+            }
+
+        }
+
+        private void btnBombilloCocina_Click(object sender, EventArgs e)
+        {
+            {
+                if (bombilloEncendido)
+                {
+                    // Apagar el bombillo (simbolizado por el icono apagado)
+                    btnBombilloCocina.Text = "💡";
+                    btnBombilloCocina.ForeColor = Color.Gray;
+                }
+                else
+                {
+                    // Encender el bombillo (color amarillo)
+
+                    btnBombilloCocina.Text = "💡   ";
+                    btnBombilloCocina.ForeColor = Color.Yellow;
+                }
+
+                bombilloEncendido = !bombilloEncendido; // Cambiar el estado
+            }
+
+        }
+
+
+
+        private void ComedorTemp_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBombilloSala_Click_1(object sender, EventArgs e)
+        {
+            {
+                if (bombilloEncendido)
+                {
+                    // Apagar el bombillo (simbolizado por el icono apagado)
+                    btnBombilloSala.Text = "💡";
+                    btnBombilloSala.ForeColor = Color.Gray;
+                }
+                else
+                {
+                    // Encender el bombillo (color amarillo)
+
+                    btnBombilloSala.Text = "💡";
+                    btnBombilloSala.ForeColor = Color.Yellow;
+                }
+
+                bombilloEncendido = !bombilloEncendido; // Cambiar el estado
+            }
+        }
+
+        private void VentanaComedor_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PuertaComedor_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VentanaHabitacion_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PuertaHabitacion_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VentanaCocina_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PuertaCocina_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VentanaSala_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PuertaSala_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
